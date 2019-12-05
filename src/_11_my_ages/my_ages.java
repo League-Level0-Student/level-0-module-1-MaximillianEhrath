64842ff0c11a5d19a75801efa317b4b1ab72e5c6
CalebVia <55600946+CalebVia@users.noreply.github.com>
2019-12-05 15:58:23 -0800

    12/5/2019

---
+++ src/_11_my_ages/my_ages.java
@@ -0,0 +1,12 @@
+package _11_my_ages;
+
+public class my_ages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i = 1; i <= 13; i++) {
+	System.out.println(i);
+}
+	}
+
+}
